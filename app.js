8f709e7760e84e5a3fb010aeabcd7dda435d49be
Logan Murphy <nagolyhpum@hotmail.com>
2016-12-05 20:25:27 -0500

    add base services

--- app.js
+++ app.js
@@ -3,6 +3,14 @@ const app = express();
 
 app.use(express.static(__dirname + "/public"));
 
+app.post("/metric", (req, res) => {
+  res.send("Not implemented yet.");
+});
+
+app.get("/metric", (req, res) => {
+  res.send("Not implemented yet.");
+});
+
 app.listen(80, () => {
   console.log("ready");
 });
