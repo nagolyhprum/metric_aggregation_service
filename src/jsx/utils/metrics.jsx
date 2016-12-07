de52df2e4b8c61f908efe7ec78e8bd4f36f13b16
Logan Murphy <nagolyhpum@hotmail.com>
2016-12-06 20:48:14 -0500

    add metrics client

---
+++ src/jsx/utils/metrics.jsx
@@ -0,0 +1,36 @@
+class Metrics {
+  constructor(fulfiller) {
+    this.fulfiller = fulfiller;
+    this.metrics = [];
+    this.locked = [];
+  }
+
+  post(metric) {
+
+    const self = this;
+
+    self.metrics.push(metric);
+
+    const promise = new Promise(resolve => {
+      if(!self.locked.length) { //if it is not locked
+        resolve(); //just resolve it
+      }
+      self.locked.push(resolve); //lock it
+    }).then(() => self.fulfiller("post", self.metrics)).then(data => {
+      self.locked.shift(); //unlock
+      self.locked[0] && self.locked[0](); //or process next
+      return data;
+    }, error => {
+      self.locked.shift(); //unlock
+      self.locked[0] && self.locked[0](); //or process next
+    });
+
+    return promise;
+  }
+
+  get(data) {
+    return this.fulfiller("get", data);
+  }
+}
+
+export default Metrics;
---
+++ test/client.jsx
@@ -0,0 +1,88 @@
+import configureMockStore from "redux-mock-store"
+import { BehaviorSubject } from "rx";
+import { createStore } from "redux";
+import { Map, List } from "immutable";
+import { expect } from "chai";
+import React from "react";
+import { mount } from "enzyme";
+
+const MockStore = (state = Map()) => configureMockStore([])(state);
+const Value = input => new BehaviorSubject(input);
+
+import Metics from "utils/metrics";
+
+const testFulfiller = value => () => {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve(value);
+    }, 10);
+  });
+};
+
+describe("metrics", () => {
+  it("can get", done => {
+    const test = { a : "b" };
+    const metrics = new Metics(testFulfiller(test));
+    metrics.get().then(data => {
+      expect(data).to.be.equal(test);
+      done();
+    });
+  });
+  it("can post", done => {
+    const test = { a : "b" };
+    const metrics = new Metics(testFulfiller(test));
+    metrics.post().then(data => {
+      expect(data).to.be.equal(test);
+      done();
+    });
+  });
+  it("will queue posts", done => {
+    const test = { a : "b" };
+    const metrics = new Metics(testFulfiller(test));
+    metrics.post().then(data => {
+      expect(data).to.be.equal(test);
+      expect(metrics.locked).to.have.length(3);
+    });
+    metrics.post().then(data => {
+      expect(data).to.be.equal(test);
+      expect(metrics.locked).to.have.length(2);
+    });;
+    metrics.post().then(data => {
+      expect(data).to.be.equal(test);
+      expect(metrics.locked).to.have.length(1);
+    });;
+    metrics.post().then(data => {
+      expect(data).to.be.equal(test);
+      expect(metrics.locked).to.have.length(0);
+      done();
+    });;
+    expect(metrics.locked).to.have.length(4);
+  });
+});
+
+/*
+import Base from "components/base";
+import {
+  setBaseValue
+} from "actions/base";
+
+describe("Base", () => {
+  it("should show hello world and 1", () => {
+    const result = mount(<Base
+      store={MockStore()}
+      base={Value("Goodbye World")}
+      index={Value(1)}
+    />);
+    const divs = result.find("div").at(0).children();
+    expect(divs.at(0).text()).to.be.equal("Goodbye World");
+    expect(divs.at(1).text()).to.be.equal("1");
+  });
+  it("has a click event", () => {
+    const store = MockStore();
+    const result = mount(<Base store={store}/>);
+    result.find("div").at(0).simulate("click");
+    expect(store.getActions()).to.have.length(1);
+    expect(store.getActions()[0]).to.be.deep.equal(setBaseValue(1));
+  });
+});
+*/
